4cb296ad3e3d70f7c44c3c974dd2d807779453ca
zyekim <kjihye72@gmail.com>
2020-10-21 12:24:52 +0900

    added banner style and header fixed style(1st)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 'use strict';
+
 $(function(){
 
+    //메인비쥬얼
+    $('.slickSlider').slick();
 
-});
+    //상단메뉴 고정
+    let header = $('#header');
+    $(window).scroll(function(){
+        if($(this).scrollTop()>0){
+            header.addClass('sticky');
+        }else{
+            header.removeClass('sticky');
+        }
+    });
+
+
+});//script
 
