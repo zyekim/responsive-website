d3ad26d10f5102cb78a47fd31185b6aa6557ac53
zyekim <kjihye72@gmail.com>
2020-10-22 18:50:20 +0900

    slick btn_1

--- js/script.js
+++ js/script.js
@@ -3,7 +3,19 @@
 $(function(){
 
     //메인비쥬얼
-    $('.slickSlider').slick();
+    $('.slickSlider').slick({
+        dots: true,
+        autoplay:true,
+    });
+
+    $('.play').on('click',function(){
+        $(this).addClass('on');
+        $('.slickSlider').slick('slickPause');
+    });
+    $('.play.on').on('click',function(){
+        $(this).removeClass('on');
+        $('.slickSlider').slick('slickPlay');
+    });
 
     //상단메뉴 고정
     let header = $('#header');
